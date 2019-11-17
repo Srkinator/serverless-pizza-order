a79c1d5b53d64f2e1e7a7ff92b566f6aa24a5ae6
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2019-11-17 17:48:10 +0100

    Implement user authentication

--- api.js
+++ api.js
@@ -1,14 +1,19 @@
+'use strict'
+
 const Api = require('claudia-api-builder')
 const api = new Api()
 
 const getPizzas = require('./handlers/get-pizzas')
-const getOrders = require('./handlers/get-orders')
 const createOrder = require('./handlers/create-order')
 const updateOrder = require('./handlers/update-order')
 const deleteOrder = require('./handlers/delete-order')
 const updateDeliveryStatus = require('./handlers/update-delivery-status')
 
+api.registerAuthorizer('userAuthentication', {
+  providerARNs: [process.env.userPoolArn]
+})
 
+// Define routes
 api.get('/', () => 'Welcome to Pizza API')
 
 api.get('/pizzas', () => {
@@ -21,35 +26,29 @@ api.get('/pizzas/{id}', (request) => {
   error: 404
 })
 
-api.get('/orders', (request) => {
-  return getOrders()
-}, {
-  error: 400
-})
-
-api.get('/orders/{id}', (request) => {
-  return getOrders(request.pathParams.id)
-}, {
-  error: 400
-})
-
 api.post('/orders', (request) => {
-  return createOrder(request.body)
+  return createOrder(request)
 }, {
   success: 201,
   error: 400
+}, {
+  cognitoAuthorizer: 'userAuthentication'
 })
 
 api.put('/orders/{id}', (request) => {
   return updateOrder(request.pathParams.id, request.body)
 }, {
   error: 400
+}, {
+  cognitoAuthorizer: 'userAuthentication'
 })
 
 api.delete('/orders/{id}', (request) => {
-  return deleteOrder(request.pathParams.id)
+  return deleteOrder(request.pathParams.id, request.context.authorizer.claims)
 }, {
   error: 400
+}, {
+  cognitoAuthorizer: 'userAuthentication'
 })
 
 api.post('delivery', (request) => {
@@ -57,7 +56,8 @@ api.post('delivery', (request) => {
 }, {
   success: 200,
   error: 400
+}, {
+  cognitoAuthorizer: 'userAuthentication'
 })
-
 
 module.exports = api
